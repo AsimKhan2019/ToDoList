e3bab170489b2b98b7ed69de8b5bc46c21aeb127
TekyRey <mwakabayah@gmail.com>
2022-07-06 09:28:11 +0300

    Style todo list

---
+++ src/modules/createLi.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    index: 1,
+    description: 'Wake up',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Morning Standup',
+    completed: true,
+  },
+  {
+    index: 3,
+    description: 'Meet with Coding Patner',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'Evening Standup',
+    completed: true,
+  },
+];
+
+export default data;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
